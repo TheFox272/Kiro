05836ed92981c93b12ce62adcc2d013a0194fc31
TheFox272 <fabien.girard27@gmail.com>
2023-11-08 19:08:09 +0100

    first commit

---
+++ ex4.jl
@@ -0,0 +1,31 @@
+using JuMP
+using GLPK
+using JSON, OrderedCollections
+
+function ex4(n::Int64)    
+    #= m = Model(GLPK.Optimizer)
+    @variable(m, z[1:n]>=0, Int)
+    @variable(m, y>=0)
+    @constraint(m, [i in 1:floor(Int,n/2)], z[i]+z[n-i+1]<=n)
+    @constraint(m, z[1]+y>=4)
+    @constraint(m, sum(z[i] for i in 1:n if rem(i, 3) == 0) <= 1)
+    @objective(m, Max, sum(z[i] for i in 1:n)-y)
+
+    # Limitation du temps de résolution à 60 secondes
+    set_time_limit_sec(m, 60.0)
+    # Résolution d’un modèle
+    optimize!(m)
+    vz = JuMP.value.(z)
+    vy = JuMP.value.(y)
+    obj = objective_value(m)
+    return vz, vy, obj =#
+    train1 = OrderedCollections.OrderedDict(
+        "0" => OrderedCollections.OrderedDict(
+            "voieAQuai" => "V2",
+            "itineraire" => "0"
+        )
+    )
+    open("test.json", "w") do f
+        JSON.print(f, train1)
+    end
+end
